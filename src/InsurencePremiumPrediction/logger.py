c06433355ecba474fd817ecfdaca0ed45050d6ad
rian <mdrianurrahaman@gmail.com>
2023-12-31 16:03:52 +0530

    logger file created

--- src/InsurencePremiumPrediction/logger.py
+++ src/InsurencePremiumPrediction/logger.py
@@ -2,18 +2,20 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+log_path=os.path.join(os.getcwd(),"logs")
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+os.makedirs(log_path,exist_ok=True)
 
+LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
+
+
+logging.basicConfig(level=logging.INFO, 
+                    filename=LOG_FILEPATH,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+                    
 )
-
 
 if __name__ == '__main__':
     logging.info("here i am AGAIN TESTING")
