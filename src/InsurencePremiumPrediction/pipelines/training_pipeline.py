27f458b2c7dd21620d4f0513d60c5149db9b8f7c
rian <mdrianurrahaman@gmail.com>
2024-01-06 00:24:51 +0530

    mlflow used in this project

--- src/InsurencePremiumPrediction/pipelines/training_pipeline.py
+++ src/InsurencePremiumPrediction/pipelines/training_pipeline.py
@@ -8,15 +8,20 @@ from sklearn.model_selection import train_test_split
 from src.InsurencePremiumPrediction.components.data_transformation import DataTransformation
 from src.InsurencePremiumPrediction.components.data_ingestion import DataIngestion
 from src.InsurencePremiumPrediction.components.model_trainer import ModelTrainer
+from src.InsurencePremiumPrediction.components.model_evaluation import ModelEvaluation
 
 
 
 
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
-    modeel_trainer=ModelTrainer()
-    modeel_trainer.initate_model_training(train_arr, test_arr)
+
+#if __name__ == "__main__":
+obj = DataIngestion()
+train_data, test_data = obj.initiate_data_ingestion()
+data_transformation = DataTransformation()
+train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+modeel_trainer=ModelTrainer()
+modeel_trainer.initate_model_training(train_arr, test_arr)
+model_eval_obj=ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+    
